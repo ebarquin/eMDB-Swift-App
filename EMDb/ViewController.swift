a1e08dae803f9a52990c39ea2c69711693bd016f
Eugenio Barquin <eu.barquin@me.com>
2017-06-12 13:26:42 +0200

    Initial Commit

---
+++ EMDb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMDb
+//
+//  Created by Eugenio Barquín on 12/6/17.
+//  Copyright © 2017 Eugenio Barquín. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '10.0'
+
+target 'EMDb' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for EMDb
+  pod 'Alamofire'
+  pod 'Kingfisher'
+  pod 'SwiftyJSON', :git => 'https://github.com/appsailor/SwiftyJSON', :branch => 'swift3' 
+
+end
