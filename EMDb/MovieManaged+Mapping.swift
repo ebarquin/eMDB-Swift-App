c4a39400c2b7b731b500f828d7c602ea9875a6ac
Eugenio Barquin <eu.barquin@me.com>
2017-06-12 17:59:29 +0200

    MovieManaged+Mapping class created to connect Movie class with MovieManaged class

---
+++ EMDb/MovieManaged+Mapping.swift
@@ -0,0 +1,17 @@
+//
+//  MovieManaged+Mapping.swift
+//  EMDb
+//
+//  Created by Eugenio Barquín on 12/6/17.
+//  Copyright © 2017 Eugenio Barquín. All rights reserved.
+//
+
+import Foundation
+
+extension MovieManaged {
+    
+    func meppedObject() -> Movie {
+        return Movie(id: self.id, title: self.title, order: Int(self.order), summary: self.summary, image: self.image, category: self.category, director: self.director)
+        
+    }
+}
